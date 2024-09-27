202807e325ce5be166c2d45c8770062124f21b5a
Fibodev <fibodev27@gmail.com>
2024-09-27 19:37:54 +0200

    build(dc):modify conditions on driven cavity

--- examples/driven-cavity.py
+++ examples/driven-cavity.py
@@ -12,14 +12,14 @@ xf = 1
 # Domain length in axis X
 Lx = np.abs(xf - x0)
 # Subintervals in axis X
-nx = 10
+nx = 5
 # Interval in axis Y
 y0 = 0
 yf = 1
 # Domain length in axis X
 Ly = np.abs(yf - y0)
 # Subintervals in axis Y
-ny = 10
+ny = 5
 
 ## Phisical Variables ##
 # Dinamic Viscosity
@@ -28,12 +28,10 @@ nu = 0.2390
 U = 0.1
 # Density
 rho = 933
-# Diameter
-D = (Lx * Ly) * 4 / (Lx + Lx + Ly + Ly)
-# Characteristic Lenght
-L = D * rho
+# Characteristic Lenght (meters)
+L = 1
 # Reynols Number
-Re = L * U / nu
+Re = 100
 
 ## Mesh ##
 x = np.linspace(x0, xf, nx+1)
@@ -48,4 +46,4 @@ def gu(x: np.ndarray, y: np.ndarray) -> np.ndarray:
 def gv(x: np.ndarray, y: np.ndarray) -> np.ndarray:
     return np.zeros_like(x)
 
-navier_stokes_2D(mesh, gu, gv, Re, 2)
+navier_stokes_2D(mesh, gu, gv, Re)
