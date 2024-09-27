de8d72b8d07f0f3272406444a0123d261c6ca553
Fibodev <fibodev27@gmail.com>
2024-09-27 19:04:07 +0200

    build(spacial): aprox spacials

--- src/aprox_spacial.py
+++ src/aprox_spacial.py
@@ -6,6 +6,9 @@ def conv_diff_u(u, v, Re, mesh):
     # Recovery dimensions
     X,Y = mesh
     nodes_y, nodes_x = X.shape
+    # Interior nodes
+    nx = nodes_x - 2
+    ny = nodes_y - 2
 
     # Output
     Fu = np.zeros((nodes_y, nodes_x))
@@ -14,13 +17,13 @@ def conv_diff_u(u, v, Re, mesh):
     dx = np.diff(X[0,:])[0]
     dy = np.diff(Y[:, 0])[0]
 
-    for i in range(1, nodes_x):
-        for j in range(1,nodes_y):
+    for i in range(1, nx):
+        for j in range(1, ny+1):
 
             u_w = (u[j,i] + u[j, i-1]) / 2
-            u_e = (u[j,i] + u[j, i+1]) / 2
-            v_n = (v[j,i] + v[j+1, i]) / 2
-            v_s = (v[j,i] + v[j-1, i]) / 2
+            u_e = (u[j,i+1] + u[j, i]) / 2
+            v_n = (v[j,i+1] + v[j, i]) / 2
+            v_s = (v[j-1,i+1] + v[j-1, i]) / 2
 
             a_w = dy * (1 / (Re * dx) + u_w / 2)
             a_e = dy * (1 / (Re * dx) + u_e / 2)
@@ -38,6 +41,9 @@ def conv_diff_v(u, v, Re, mesh):
     # Recovery dimensions
     X,Y = mesh
     nodes_y, nodes_x = X.shape
+    # Interval numbers
+    nx = nodes_x - 1
+    ny = nodes_y - 1
 
     # Output
     Fv = np.zeros((nodes_y, nodes_x))
@@ -46,12 +52,12 @@ def conv_diff_v(u, v, Re, mesh):
     dx = np.diff(X[0,:])[0]
     dy = np.diff(Y[:, 0])[0]
 
-    for i in range(1, nodes_x):
-        for j in range(1,nodes_y):
+    for i in range(1, nx):
+        for j in range(1,ny):
 
-            u_w = (u[j,i] + u[j, i-1]) / 2
-            u_e = (u[j,i] + u[j, i+1]) / 2
-            v_n = (v[j,i] + v[j+1, i]) / 2
+            u_w = (u[j+1,i-1] + u[j, i-1]) / 2
+            u_e = (u[j+1,i] + u[j, i]) / 2
+            v_n = (v[j+1,i] + v[j, i]) / 2
             v_s = (v[j,i] + v[j-1, i]) / 2
 
             a_w = dy * (1 / (Re * dx) + u_w / 2)
@@ -72,6 +78,9 @@ def gradient(p, mesh):
     # Recovery dimensions
     X,Y = mesh
     nodes_y, nodes_x = X.shape
+    # Interior nodes
+    nx = nodes_x - 2
+    ny = nodes_y - 2
 
     # Hinges
     dx = np.diff(X[0,:])[0]
@@ -81,9 +90,19 @@ def gradient(p, mesh):
     grad_pu = np.zeros((nodes_y, nodes_x))
     grad_pv = np.zeros((nodes_y, nodes_x))
 
-    for i in range(1, nodes_x):
-        for j in range(1,nodes_y):
-            grad_pu[i,j] = (p[j,i+1] - p[j,i-1]) / (2 * dx)
-            grad_pv[i,j] = (p[j+1,i] - p[j-1,i]) / (2 * dy)
+    for i in range(1, nx+1):
+        for j in range(1,ny+1):
+            grad_pu[j,i] = (p[j,i+1] - p[j,i-1]) / (2 * dx)
+            grad_pv[j,i] = (p[j+1,i] - p[j-1,i]) / (2 * dy)
+    
+    ## Boundary conditions ##
+    # U
+    for j in range(ny+2):
+        grad_pu[j,0] = (p[j,1] - p[j,0]) / dx
+        grad_pu[j,nx+1] = (p[j,nx+1] - p[j,nx]) / dx
+    # V
+    for i in range(nx+2):
+        grad_pv[0,i] = (p[1,i] - p[0,i]) / dy
+        grad_pv[ny+1,i] = (p[ny+1,i] - p[ny,i]) / dy
 
     return (grad_pu, grad_pv)  
