47c0cdf1f955ea62119e464870634d49e7c0a037
Fibodev <fibodev27@gmail.com>
2024-10-22 11:15:51 +0200

    fix(frac):remove fractions, instead decimals

--- src/aprox_temporal.py
+++ src/aprox_temporal.py
@@ -15,8 +15,8 @@ def intermediate_velocity_adamsB(u , v, Re, mesh, Fu_old, Fv_old, dt):
     Fu = conv_diff_u(u, v, Re, mesh)
     Fv = conv_diff_v(u, v, Re, mesh)
 
-    u_star = u + dt * ((3/2) * Fu - (1/2) * Fu_old)
-    v_star = v + dt * ((3/2) * Fv - (1/2) * Fv_old)
+    u_star = u + dt * (1.5 * Fu - 0.5 * Fu_old)
+    v_star = v + dt * (1.5 * Fv - 0.5 * Fv_old)
 
     return (u_star, v_star, Fu, Fv)
 
