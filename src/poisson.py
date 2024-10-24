25afcf146060b1229923a8e0727fb0bec74118d4
Fibodev <fibodev27@gmail.com>
2024-10-24 16:44:32 +0200

    refactor(poisson):solve equation only in interior points

--- src/poisson.py
+++ src/poisson.py
@@ -10,10 +10,9 @@ def poisson_2D(nx, ny, A, u_star, v_star, p, dx, dy, dt):
     """
     #Second Member (Divergence of Intermediate Velocities)
     b = divergence(u_star, v_star, dx, dy, nx, ny) / dt
-
-    #intermediate_divergence((X[1:ny+1,1:nx+1], Y[1:ny+1, 1:nx+1]), divergence(u_star, v_star, dx, dy, nx, ny))
-
-    b = b.reshape(nx*ny)
+    
+    n = int(nx * ny)
+    b = b.reshape(n)
 
     # Resolution on Interior Nodes
     LU = splu(A)
@@ -21,7 +20,7 @@ def poisson_2D(nx, ny, A, u_star, v_star, p, dx, dy, dt):
     p_star = p_star.reshape((ny,nx))
 
     # Update pressure
-    p[1:ny+1, 1:nx+1] = p_star.copy()
+    p[1:-1,1:-1] = p_star.copy()
     p[0,:] = p[1,:]
     p[ny+1,:] = p[ny,:]
     p[:,0] = p[:,1]
